c86389fd33eff68546ffe4c63427e44fa65bf59c
Kgee321 <geek@middleton.school.nz>
2022-03-20 13:26:44 +1300

    Main function ended

--- Paper Scissors Rock/main function.py
+++ Paper Scissors Rock/main function.py
@@ -3,19 +3,20 @@ Also calls on random and makes the computer randomly choose an element
 Katelyn Gee
 19/03/2022"""
 
+import random
+
 
 def main_function():
     element = input("Enter either P for Paper, S for Scissors and R for Rock: ").upper()
-    import random
-
-
-
-
-
-
-
-
-
+    computer = random.choice("RPS")
+    if element != "P" or element != "R" or element != "S":
+        print("That was not a correct input: Try again")
+        main_function()
+    if element == computer:
+        print(f"You choice {element} and computer choice {computer}. It was a draw: Try again")
+        main_function()
+    else:
+        winner()
 
 
 print("--------- Paper Scissors Rock ------------")
